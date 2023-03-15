73ab4b2b841ea6384ebd694b207090f96e56fb70
Abyl Shaiymbetov <119596574+darkusthonos@users.noreply.github.com>
2023-03-15 16:50:37 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db ,
+load_job_from_db
 
 app = Flask(__name__)
 
@@ -17,6 +18,11 @@ def hello_kochkor():
 def list_lobs():
   jobs= load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_jobs_from_db(id)
+  return jsontify(job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
