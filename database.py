ae95317ec4041d3014063b110f170fd5ecd5ab36
Abyl Shaiymbetov <119596574+darkusthonos@users.noreply.github.com>
2023-03-17 16:00:21 +0000

    final work has been done

--- database.py
+++ database.py
@@ -1,20 +1,17 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string=os.environ ['DB_CONNECTION_STRING']
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-
-
-engine = create_engine(db_connection_string,connect_args={
+engine = create_engine(db_connection_string, connect_args={
   "ssl":{
     "ssl_ca":"/etc/ssl/cert.pem"
-  
   }
 }) 
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))  
+    result = conn.execute(text("SELECT * FROM jobs"))  
     jobs = []
     for row in result.all():
       row_dict = dict(zip(result.keys(), row))
@@ -25,7 +22,7 @@ def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
       text(f"SELECT * FROM jobs WHERE id={id}")
-      )
+    )
     rows = []
     for row in result.all():
       rows.append(row._mapping)
@@ -33,3 +30,25 @@ def load_job_from_db(id):
       return None
     else:
       return dict(rows[0])
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        conn.execute(query, {
+            "job_id": job_id,
+            "full_name": data['full_name'],
+            "email": data['email'],
+            "linkedin_url": data['linkedin_url'],
+            "education": data['education'],
+            "work_experience": data['work_experience'],
+            "resume_url": data['resume_url']
+        })
+
+
+
+    
+
+
+
+                 
+     
